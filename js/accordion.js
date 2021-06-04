0b97b48a2f69c99c1efe992dd6d662602e39c0d2
unknown <abdoennaddam@gmail.com>
2021-06-04 01:01:19 +0100

    jarida international scientific journals

---
+++ js/accordion.js
@@ -0,0 +1,39 @@
+// Create a condition that targets viewports at least 768px wide
+const smallQuery = window.matchMedia("(max-width : 50em)");
+
+function smallMedia(m) {
+  // Check if the media query is true
+  if (m.matches) {
+     // Then make the footer looks good
+    var acc = document.getElementsByClassName("accordion");
+    var i;
+
+    for (i = 0; i < acc.length; i++) {
+      acc[i].nextElementSibling.style.display = "none";
+
+      acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+          panel.style.display = "none";
+        } else {
+          panel.style.display = "block";
+        }
+      });
+    }
+  }
+  else
+  {
+    var acc = document.getElementsByClassName("accordion");
+    var i;
+    for (i = 0; i < acc.length; i++) {
+      acc[i].nextElementSibling.style.display = "block";
+      // acc[i].classList.remove("active");
+    }
+  }
+}
+// Register event listener
+smallQuery.addListener(smallMedia);
+
+// Initial check
+smallMedia(smallQuery);
